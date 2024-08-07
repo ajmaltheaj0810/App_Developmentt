7d78cf71b261f7cbcf98a50c94890f4a9e062251
ajmaltheaj0810 <153708775+ajmaltheaj0810@users.noreply.github.com>
2024-08-07 10:14:03 +0530

    Add files via upload

---
+++ frontend/src/componenets/dashboard/Admin.jsx
@@ -0,0 +1,43 @@
+import { React } from 'react';
+
+
+
+function Admin(){
+
+  return(
+    <div class="admin-body">
+      <div className='header'>
+        <div>DashBoard</div>
+        <div>Users</div>
+        <div>About</div>
+        <div>Sign Out</div>
+      </div>
+        <div class='Appbar'>
+          headder
+        </div>
+      <div className='dashboard'>
+        <div class='st'>
+        <div>
+          Available
+        </div>
+        <div>
+          Booked
+        </div>
+        <div>
+          Cancelled
+        </div>
+        <div>Total
+
+        </div>
+      </div>
+        <div class='admin-status'>
+            Status
+        </div>
+        <div class='admin-chart'>
+            Chart
+        </div>
+</div>
+    </div>
+  );
+}
+export default Admin;
